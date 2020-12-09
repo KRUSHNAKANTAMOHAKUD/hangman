109f55e0eb6027941936921ed26fc73eaf38b53f
Krushnakanta Mohakud <krushnakanta@orchids.edu.in>
2020-12-09 11:39:49 +0530

    Updates

---
+++ precache-manifest.5f04f73ed7c0b0c3e1bc2560dedd5a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8eb81d1d10b07e6acf1538539961ee5",
+    "url": "/hangman_live/index.html"
+  },
+  {
+    "revision": "72da63b1e6d20bd0d59e",
+    "url": "/hangman_live/static/css/main.0998170a.chunk.css"
+  },
+  {
+    "revision": "f3293ff065b04177a034",
+    "url": "/hangman_live/static/js/2.0e429a6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman_live/static/js/2.0e429a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72da63b1e6d20bd0d59e",
+    "url": "/hangman_live/static/js/main.d4141841.chunk.js"
+  },
+  {
+    "revision": "113858202bfcadb3ef21",
+    "url": "/hangman_live/static/js/runtime-main.365e3cc4.js"
+  }
+]);
